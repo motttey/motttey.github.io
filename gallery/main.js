a73944c099122851b93c8e0e07198a30d2f8cba4
motttey <motitago@gmail.com>
2020-03-07 01:15:06 +0900

    add details of illust

--- gallery/main.js
+++ gallery/main.js
@@ -8,6 +8,7 @@ const canvas_height = 650;
 const canvas_offset_x = 200;
 
 let target_images = [];
+const axes = ["tsne-X", "tsne-Y", "tsne-Z"];
 
 // create the scene
 let scene = new THREE.Scene();
@@ -28,7 +29,6 @@ function drawScatter(){
   renderer.setSize(canvas_width, canvas_height);
 
   // add the DOM element that the renderer will draw to to the page
-  console.log(document.body)
   let canvas = document.getElementById("canvas_holder");
   canvas.appendChild(renderer.domElement);
   // set the background color to almost-white
@@ -59,6 +59,7 @@ function drawScatter(){
     v(-box_size, box_size, -box_size), v(-box_size, box_size, box_size),
     v(-box_size, -box_size, -box_size), v(-box_size, -box_size, box_size)
   );
+
   let lineMat = new THREE.LineBasicMaterial({color: 0xFFFFFF, linewidth: 1});
   let line = new THREE.Line(lineGeo, lineMat);
   line.type = THREE.Lines;
@@ -69,8 +70,6 @@ function drawScatter(){
     target_images = data.slice(0,100);
     console.log(target_images);
 
-    let coordinate_bounds = {}
-    const axes = ["tsne-X", "tsne-Y", "tsne-Z"];
     axes.forEach((axis, i) => {
       coordinate_bounds[axis] = [
         Math.max.apply(Math, target_images.map(function(o) { return o[axis]; })),
@@ -82,7 +81,6 @@ function drawScatter(){
       const loader = new THREE.TextureLoader();
 	    loader.crossOrigin = true;
 
-      // let texture = loader.load("http://embed.pixiv.net/decorate.php?illust_id=" + d["id"] + "&mode=sns-automator");
       let texture = loader.load("./thumbnails/" + d["id"] + ".png");
 
       let mat = new THREE.PointsMaterial(
@@ -112,7 +110,6 @@ function drawScatter(){
       scatterPlot.add(points);
     });
     console.log(scene.children);
-
   });
 
   // add a spotlight to the scene
@@ -124,67 +121,68 @@ function drawScatter(){
   // animate(new Date().getTime() * 0.0001);
 	document.addEventListener( 'mousemove', onDocumentMouseMove, false );
   window.requestAnimationFrame(animate, renderer.domElement);
+}
 
-  function animate(t) {
-    // update the aspect ratio and renderer size in case the window was resized
-    camera = new THREE.PerspectiveCamera( 40, canvas_width / canvas_height, 0.3, 2000 );
-    renderer.setSize(canvas_width, canvas_height);
-    // spin the camera in a circle
-    camera.position.x = Math.sin(t/3000)*250;
-    camera.position.y = 100;
-    camera.position.z = Math.cos(t/3000)*250;
-    // point the camera at the origin
-    camera.lookAt(scene.position);
-    // render the scene again
-    renderer.setViewport(-1 * canvas_offset_x, 0,  canvas_width, canvas_height);
-    renderer.render(scene, camera);
-    // request the next animation frame to render again
-    window.requestAnimationFrame(animate, renderer.domElement);
-  };
+function animate(t) {
+  // update the aspect ratio and renderer size in case the window was resized
+  camera = new THREE.PerspectiveCamera( 40, canvas_width / canvas_height, 0.3, 2000 );
+  renderer.setSize(canvas_width, canvas_height);
+  // spin the camera in a circle
+  camera.position.x = Math.sin(t/3000)*250;
+  camera.position.y = 100;
+  camera.position.z = Math.cos(t/3000)*250;
+  // point the camera at the origin
+  camera.lookAt(scene.position);
+  // render the scene again
+  renderer.setViewport(-1 * canvas_offset_x, 0,  canvas_width, canvas_height);
+  renderer.render(scene, camera);
+  // request the next animation frame to render again
+  window.requestAnimationFrame(animate, renderer.domElement);
+};
 
-  function onDocumentMouseMove( event )
-  {
-  	// the following line would stop any other event handler from firing
-  	// (such as the mouse's TrackballControls)
-  	// event.preventDefault();
-    let raycaster = new THREE.Raycaster();
+function onDocumentMouseMove( event )
+{
+  // the following line would stop any other event handler from firing
+  // (such as the mouse's TrackballControls)
+  // event.preventDefault();
+  let raycaster = new THREE.Raycaster();
 
-    let mouse = THREE.Vector2;
-    let mousePosition = calcMousePositionInCanvas(renderer.domElement, event);
-    mouse.x = ((mousePosition[0] + canvas_offset_x) / canvas_width) * 2 - 1;
-    mouse.y = - (mousePosition[1] / canvas_height) * 2 + 1;
-    raycaster.setFromCamera(mouse, camera);
+  let mouse = THREE.Vector2;
+  let mousePosition = calcMousePositionInCanvas(renderer.domElement, event);
+  mouse.x = ((mousePosition[0] + canvas_offset_x) / canvas_width) * 2 - 1;
+  mouse.y = - (mousePosition[1] / canvas_height) * 2 + 1;
+  raycaster.setFromCamera(mouse, camera);
 
-    let intersects = raycaster.intersectObjects(scene.children[0].children.slice(1), true);
+  let intersects = raycaster.intersectObjects(scene.children[0].children.slice(1), true);
 
-    // scene.add(new THREE.ArrowHelper(raycaster.ray.direction, raycaster.ray.origin, 300, 0xff0000) );
+  // scene.add(new THREE.ArrowHelper(raycaster.ray.direction, raycaster.ray.origin, 300, 0xff0000) );
 
-    // console.log(intersects);
-    if (intersects.length>0){
-        intersects.forEach((item, i) => {
-          // console.log('name is:' + item.object.name);
-          // item.object.material.color.setHex( Math.random() * 0xffffff);
+  // console.log(intersects);
+  if (intersects.length>0){
+      intersects.forEach((item, i) => {
+        // console.log('name is:' + item.object.name);
+        // item.object.material.color.setHex( Math.random() * 0xffffff);
 
-          let target_image = target_images.filter(img => img["id"] == item.object.name);
+        let target_image = target_images.filter(img => img["id"] == item.object.name);
 
-          document.getElementById("image_thumb").src = "http://embed.pixiv.net/decorate.php?illust_id=" + item.object.name + "&mode=sns-automator";
+        document.getElementById("image_thumb").src = "http://embed.pixiv.net/decorate.php?illust_id=" + item.object.name + "&mode=sns-automator";
 
-          if (target_image.length > 0){
-            document.getElementById("image_title").innerHTML = target_image[0]["title"];
-            document.getElementById("image_tags").innerHTML = target_image[0]["tags"].map( (tag) => "#" + tag["name"]).join(",");
-          }
-        });
+        if (target_image.length > 0){
+          document.getElementById("image_title").innerHTML = target_image[0]["title"];
+          document.getElementById("image_date").innerHTML = target_image[0]["date"].toString();
+          document.getElementById("image_tags").innerHTML = target_image[0]["tags"].map( (tag) => "#" + tag["name"]).join(" ");
 
-        console.log("Intersected object:", intersects.length);
-    }
+          document.getElementById("image_url").href = "https://www.pixiv.net/artworks/" + target_image[0]["id"];
+        }
+      });
   }
+}
 
-  function calcMousePositionInCanvas(canvas, event) {
-    let pos_x, pos_y;
+function calcMousePositionInCanvas(canvas, event) {
+  let pos_x, pos_y;
 
-    let offset = $(canvas).offset();
-    pos_x = event.clientX + document.body.scrollLeft + document.documentElement.scrollLeft - Math.floor(offset.left);
-    pos_y = event.clientY + document.body.scrollTop + document.documentElement.scrollTop - Math.floor(offset.top);
-    return [pos_x, pos_y];
-  }
+  let offset = $(canvas).offset();
+  pos_x = event.clientX + document.body.scrollLeft + document.documentElement.scrollLeft - Math.floor(offset.left);
+  pos_y = event.clientY + document.body.scrollTop + document.documentElement.scrollTop - Math.floor(offset.top);
+  return [pos_x, pos_y];
 }
