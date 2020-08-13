f675e6e257e2ad2e8bb0ba68cfd97dc0249cd1e9
motttey <motitago@gmail.com>
2020-08-13 21:24:16 +0900

    add layout of works

--- components/LinkCards.vue
+++ components/LinkCards.vue
@@ -3,31 +3,35 @@
     <h1>My Links</h1>
     <h3>パーソナリティを知ってください. </h3>
 
-     <v-row dense class="mb-6">
-       <v-col
-         v-for="card in cards"
-         :key="card.title"
-         :cols="card.flex"
+    <v-row dense class="mb-6">
+      <v-col cols="12" sm="10" offset-sm="1"
        >
-         <v-card>
-           <v-img
-             :src="card.src"
-             class="white--text align-end"
-             gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
-             height="200px"
-           >
-            <v-card-title v-text="card.title"></v-card-title>
-           </v-img>
+        <v-container fluid>
+          <v-row>
+            <v-col :cols="card.flex"
+              v-for="card in cards"
+              :key="card.tditle">
+              <v-card>
+                <v-img
+                  :src="card.src"
+                  class="white--text align-end"
+                  gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+                  height="200px"
+                >
+                  <v-card-title v-text="card.title"></v-card-title>
+                </v-img>
 
-           <v-card-actions>
-             <v-spacer></v-spacer>
-           </v-card-actions>
+                <v-card-actions>
+                 <v-spacer></v-spacer>
+                </v-card-actions>
 
-           <v-card-text>
-             <a :href="card.url">Link</a>
-           </v-card-text>
-
-         </v-card>
+                <v-card-text>
+                  <a :href="card.url">Link</a>
+                </v-card-text>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-container>
        </v-col>
      </v-row>
    </v-container>
--- components/Works.vue
+++ components/Works.vue
@@ -2,7 +2,83 @@
   <v-container fluid>
     <h1>Works</h1>
     <h3>同人誌やポートフォリオにしたいですね. </h3>
-    <v-row dense class="mb-6">
-    </v-row>
+    <v-row dense class="my-6">
+       <v-col cols="12" sm="8" offset-sm="2">
+         <v-card class="mb-6">
+           <v-container fluid>
+             <v-row>
+               <v-col
+                 v-for="card in cards"
+                 :key="n"
+                 class="d-flex child-flex"
+                 cols="4"
+               >
+                 <v-card flat>
+                   <v-img
+                     :src="card.src"
+                     :lazy-src="card.src"
+                     aspect-ratio="1"
+                     class="grey lighten-2"
+                     gradient="to bottom, rgba(0,0,0,.5), rgba(0,0,0,.1)"
+                   >
+                     <template>
+                       <v-card-text>
+                         <h2>{{card.title}}</h2>
+                       </v-card-text>
+                     </template>
+                     <template v-slot:placeholder>
+                       <v-row
+                         class="fill-height ma-0"
+                         align="center"
+                         justify="center"
+                       >
+                         <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
+                       </v-row>
+                     </template>
+                   </v-img>
+                   <!--
+                   <template>
+                     <v-card-actions>
+                      <v-spacer></v-spacer>
+                     </v-card-actions>
+                     <v-card-text>
+                       <a :href="card.url">Link</a>
+                     </v-card-text>
+                   </template>
+                  -->
+                 </v-card>
+               </v-col>
+             </v-row>
+           </v-container>
+         </v-card>
+       </v-col>
+     </v-row>
   </v-container>
 </template>
+
+<script>
+  export default {
+    data: () => ({
+      cards: [
+        { title: 'Perfect Blue', src: '/mochiduko-20/doraemon-namecard.png',
+        url: 'https://www.pixiv.net/users/415546', flex: 6 },
+        { title: 'DORA THE PAST', src: '/mochiduko-20/doraemon-namecard.png',
+        url: 'https://www.deviantart.com/motttey', flex: 6 },
+        { title: 'weibo',  src: '/mochiduko-20/doraemon-namecard.png',
+        url: 'https://weibo.com/7310121728', flex: 6 },
+        { title: 'pixiv', src: '/mochiduko-20/doraemon-namecard.png',
+        url: 'https://www.pixiv.net/users/415546', flex: 6 },
+        { title: 'deviantART', src: '/mochiduko-20/doraemon-namecard.png',
+        url: 'https://www.deviantart.com/motttey', flex: 6 },
+        { title: 'weibo',  src: '/mochiduko-20/doraemon-namecard.png',
+        url: 'https://weibo.com/7310121728', flex: 6 },
+        { title: 'pixiv', src: '/mochiduko-20/doraemon-namecard.png',
+        url: 'https://www.pixiv.net/users/415546', flex: 6 },
+        { title: 'deviantART', src: '/mochiduko-20/doraemon-namecard.png',
+        url: 'https://www.deviantart.com/motttey', flex: 6 },
+        { title: 'weibo',  src: '/mochiduko-20/doraemon-namecard.png',
+        url: 'https://weibo.com/7310121728', flex: 6 },
+      ],
+    }),
+  }
+</script>
