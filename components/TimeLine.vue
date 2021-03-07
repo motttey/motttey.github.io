abfd0215747ebc6eeb14fcc7e34a65ff02602eb8
motttey <motitago@gmail.com>
2021-03-07 21:05:53 +0900

    fix timeline

--- components/TimeLine.vue
+++ components/TimeLine.vue
@@ -1,8 +1,10 @@
 <template>
   <v-timeline>
     <!-- 自己紹介など -->
-    <v-timeline-item color="#0288D1">1995: Born in Toyama, Japan</v-timeline-item>
+    <v-timeline-item color="#0288D1">1995: Born in Toyama, Japan. </v-timeline-item>
     <v-timeline-item class="text-right" color="#0288D1">2008: Start drawing</v-timeline-item>
-    <v-timeline-item color="#0288D1">2020: Open this site</v-timeline-item>
+    <v-timeline-item color="#0288D1">2009: Start twitter</v-timeline-item>
+    <v-timeline-item class="text-right" color="#0288D1">2020: Open this site</v-timeline-item>
+    <v-timeline-item color="#ffffff">2021: Currently, I'm living in Kanagawa, Japan. </v-timeline-item>
   </v-timeline>
 </template>
