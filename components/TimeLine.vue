e64a8f93d7436780da6fbefda58625033b7d43fc
motttey <motitago@gmail.com>
2021-03-30 21:33:51 +0900

    fix timeline

--- components/TimeLine.vue
+++ components/TimeLine.vue
@@ -5,6 +5,6 @@
     <v-timeline-item class="text-right" color="#0288D1">2008: Start drawing</v-timeline-item>
     <v-timeline-item color="#0288D1">2009: Start twitter</v-timeline-item>
     <v-timeline-item class="text-right" color="#0288D1">2020: Open this site</v-timeline-item>
-    <v-timeline-item color="#ffffff">2021: Currently, I'm living in Kanagawa, Japan. </v-timeline-item>
+    <v-timeline-item color="#ffffff">2021: Currently, I'm living in Kanagawa, Japan. And I'm working as Software Engineer. </v-timeline-item>
   </v-timeline>
 </template>
