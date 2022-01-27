1ec83916277a519b629b54f91142d3d9b2c5126d
motttey <motitago@gmail.com>
2022-01-27 21:01:31 +0900

    add: link page

---
+++ pages/link/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>リンク集</h1>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      links: [
+        {
+          title: "モチヅ庫"
+        }
+      ]
+    }
+  }
+}
