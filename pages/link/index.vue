7802848aad3d1fc0167607499a1ad6ee7161b96a
motttey <motitago@gmail.com>
2022-02-18 21:49:44 +0900

    add: v-data-table on link page

--- pages/link/index.vue
+++ pages/link/index.vue
@@ -1,16 +1,58 @@
 <template>
-  <h1>リンク集</h1>
+  <v-layout
+    column
+    justify-center
+    align-center
+  >
+    <v-flex
+      xs12
+      sm6
+      md6
+    >
+      <h1>リンク集</h1>
+      <v-data-table
+        :headers="headers"
+        :items="links"
+        :items-per-page="50"
+        item-key="id"
+        class="link-table"
+      >
+        <template v-slot:[`item.title`]="{ item }">
+          <a :href="item.url" target="_blank">
+            {{ item.title }}
+          </a>
+        </template>
+      </v-data-table>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
 export default {
-  data () {
-    return {
-      links: [
+  name: "linkTable",
+  data: () => ({
+    links: [
+      {
+        id: 1,
+        title: "モチヅ庫",
+        url: "https://motttey.github.io/",
+        description: "私のサイトです."
+      }
+    ]
+  }),
+  computed: {
+    headers() {
+      return [
         {
-          title: "モチヅ庫"
+          text: "タイトル",
+          value: "title"
+        },
+        {
+          text: "紹介",
+          value: "description"
         }
       ]
     }
   }
 }
+</script>
