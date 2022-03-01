e728e848753b749f24ca80d4d23be565ba47cfcd
motttey <motitago@gmail.com>
2022-03-01 21:28:36 +0900

    add: DataTable.vue

---
+++ components/DataTable.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-data-table
+    :headers="headers"
+    :items="links"
+    :items-per-page="15"
+    item-key="id"
+    class="link-table"
+    v-if="links.length > 0"
+  >
+    <template v-slot:[`item.title`]="{ item }">
+      <a :href="item.url" target="_blank">
+        {{ item.title }}
+      </a>
+    </template>
+  </v-data-table>
+</template>
+
+
+<script>
+export default {
+  props: {
+    links: {
+      type: Array,
+      default: () => [],
+      required: true
+    }
+  },
+  computed: {
+    headers() {
+      return [
+        {
+          text: "タイトル",
+          value: "title"
+        },
+        {
+          text: "カテゴリ",
+          value: "category"
+        },
+        {
+          text: "紹介",
+          value: "description"
+        }
+      ]
+    }
+  }
+}
+</script>
