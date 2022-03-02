0f778f8f051c3001c2526a32b97ca531a56dc68e
motttey <motitago@gmail.com>
2022-03-02 20:57:44 +0900

    fix: table styles

--- components/DataTable.vue
+++ components/DataTable.vue
@@ -1,5 +1,6 @@
 <template>
   <v-data-table
+    dense
     :headers="headers"
     :items="links"
     :items-per-page="15"
@@ -8,7 +9,11 @@
     v-if="links.length > 0"
   >
     <template v-slot:[`item.title`]="{ item }">
-      <a :href="item.url" target="_blank">
+      <a
+        :href="item.url"
+        target="_blank"
+        class="font-weight-bold"
+      >
         {{ item.title }}
       </a>
     </template>
@@ -45,3 +50,9 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+  .theme--dark.v-data-table {
+    background-color: rgba(30, 30, 30, 0.5);
+  }
+</style>
