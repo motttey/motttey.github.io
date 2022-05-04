4c590fda4930014f15021ef2b77bb6c14a20e2b3
motttey <motitago@gmail.com>
2022-05-04 21:02:50 +0900

    add stories dir

---
+++ pages/stories/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-layout
+    column
+    justify-center
+    align-center
+  >
+    <v-flex
+      xs12
+      sm9
+      md9
+    >
+      <v-container fluid>
+        <v-row>
+          <h1>コミックス概説</h1>
+        </v-row>
+        <v-row>
+          <v-col>
+            <div>
+              キーワードの一覧を配置
+            </div>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  name: "Stories",
+  data: () => ({
+  })
+}
